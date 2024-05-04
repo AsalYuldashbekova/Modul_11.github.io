a14e5ee035c53a2702f2cc695372e9f3689d2bc6
AsalYuldashbekova <asalyuldashbekova@gmail.com>
2024-05-04 14:41:33 +0500

    update project

--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -1,7 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-
-web_app = WebAppInfo(url="https://www.google.com")
+web_app = WebAppInfo(url="https://asalyuldashbekova.github.io/Modul_11.github.io/")
 
 app_ikb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Mini App", web_app=web_app)]
