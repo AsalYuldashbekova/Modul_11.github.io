c9f3bd074f7686ee9e12ba558f3dc49a27ab3084
AsalYuldashbekova <asalyuldashbekova@gmail.com>
2024-05-11 16:07:21 +0500

    Update my_bot

--- app.js
+++ app.js
@@ -4,34 +4,48 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF"
 tg.MainButton.color = "#FC3005"
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
+let rbtn1 = document.getElementById("rbtn1");
+let abtn1 = document.getElementById("abtn1");
+
+let rbtn2 = document.getElementById("rbtn2");
+let abtn2 = document.getElementById("abtn2");
+
+let rbtn3 = document.getElementById("rbtn3");
+let abtn3 = document.getElementById("abtn3");
+
+let rbtn4 = document.getElementById("rbtn4");
+let abtn4 = document.getElementById("abtn4");
 
 let item = " "
+let num_count = 0
 
+number = document.getElementById("count");
 
+abtn1.addEventListener("click", function () {
+
+    number.innerText = num_count += 1;
+    number.style.display = "block";
+})
 
 
 btn1.addEventListener("click", function () {
-    tg.MainButton.setText("Burger");
-    item = "Burger /3$";
+    tg.MainButton.setText("Burger " + num);
+    item = "Burger/3$";
     tg.MainButton.show();
 });
 btn2.addEventListener("click", function () {
-    tg.MainButton.setText("KFS");
-    item = "KFS /5$";
+    tg.MainButton.setText("KFC");
+    item = "KFC/10$";
     tg.MainButton.show();
 });
 btn3.addEventListener("click", function () {
-    tg.MainButton.setText("Lavash");
-    item = "Lavash /4$";
+    tg.MainButton.setText("LAVASH");
+    item = "LAVASH/4$";
     tg.MainButton.show();
 });
 btn4.addEventListener("click", function () {
     tg.MainButton.setText("Pizza");
-    item = "Pizza /6$";
+    item = "Pizza/5$";
 
     tg.MainButton.show();
 });
