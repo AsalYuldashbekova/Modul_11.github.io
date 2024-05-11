c84d25214dc23ae0a12889ca03198dae4a780027
AsalYuldashbekova <asalyuldashbekova@gmail.com>
2024-05-11 15:29:23 +0500

    Update my_bot

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -8,3 +8,10 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await message.answer("Salom", reply_markup=app_ikb)
+
+
+@dp.message(F.func(lambda message: message.web_app_data.data))
+async def get_btn(message: types.Message):
+    text = message.web_app_data.data
+    await message.answer(text=f"Nomi: {text.split('/')[0]}\n"
+                              f"Narxi: {text.split('/')[1]}")
